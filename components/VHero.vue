9ccb9b8c4eff01c8b00293e7df08bd7dbcec63a2
Lucas Fontes Gaspareto <lucasfontesgaspareto@gmail.com>
2019-06-22 23:15:23 -0300

    update VHero.vue add login page as entrar.vue

--- components/VHero.vue
+++ components/VHero.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="sm:mx-4 my-10 container flex xl:mx-auto">
-    <div class="text-center sm:text-left md:w-1/2">
-      <h1 class="mx-5 sm:mx-0 text-4xl md:text-5xl font-bold">Administre seus investimentos de forma inteligente</h1>
-      <h3 class="my-10 text-medium font-medium">Um jeito eficiente de registrar suas ordens e ótimos recursos parar analisar seus investimentos</h3>
+  <div class="container flex xl:mx-auto">
+    <div class="text-center lg:text-left lg:w-2/3">
+      <h1 class="m-5 text-4xl md:text-5xl font-bold">Administre seus investimentos de forma inteligente</h1>
+      <h3 class="m-5 mb-10 text-medium md:text-2xl font-medium">Um jeito eficiente de registrar suas ordens e ótimos recursos parar analisar seus investimentos</h3>
       <a
-        href="#"
+        href="entrar"
         class="shadow-xl focus:shadow-none rounded-full mx-4 py-3 px-8 text-white bg-green-400 hover:bg-green-500 font-medium text-base align-bottom leading-normal"
         >Entrar</a
       >
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
     <v-header></v-header>
-    <!-- <v-hero></v-hero> -->
+    <v-hero></v-hero>
   </div>
 </template>
